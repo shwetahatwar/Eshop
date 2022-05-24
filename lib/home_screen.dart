a64618adb47d7b85d6323045d1cc944ffbde4bed
unknown <shweta.hatwar6@gmail.com>
2022-05-24 16:24:09 +0530

    Menu drawer added on home screen with menu

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'Home/dashboard.dart';
 import 'login-screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,18 +15,108 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
-              print("Signed Out");
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SignInScreen()));
-            });
-          },
+      appBar: AppBar(title: Text('title')),
+      body: const Center(
+        child: Text('My Page!'),
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Eshopping'),
+            ),
+            ListTile(
+              title: const Text('Eshop'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Buy Now'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Orders'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Favorites'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Payments'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('About Us'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Logout'),
+              onTap: () {
+                FirebaseAuth.instance.signOut().then((value) {
+                  print("Signed Out");
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignInScreen()));
+                });
+              }
+            ),
+          ],
         ),
       ),
     );
+
+
+
+    // return Scaffold(
+    //   body: Center(
+    //     child: ElevatedButton(
+    //       child: Text("Logout"),
+    //       onPressed: () {
+    //         FirebaseAuth.instance.signOut().then((value) {
+    //           print("Signed Out");
+    //           Navigator.push(context,
+    //               MaterialPageRoute(builder: (context) => MenuDrawer()));
+    //         });
+    //       },
+    //     ),
+    //   ),
+    // );
   }
 }
