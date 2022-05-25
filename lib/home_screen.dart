8652b5a1148e754c08f9bfb88473693436afedee
unknown <shweta.hatwar6@gmail.com>
2022-05-25 11:29:57 +0530

    Search Icon added on top of dashboard

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,10 +15,15 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('title')),
-      body: const Center(
-        child: Text('My Page!'),
+      appBar: AppBar(title: Text('title'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+        ],
       ),
+
+      // body: const Center(
+      //   child: Text('My Page!'),
+      // ),
       drawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
